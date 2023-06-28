e108eeaf4f07c5ec161155e2a26560c9c9bc363a
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2023-06-28 18:44:49 +0330

    add shell file for executing the app

--- code/internal/services/bepa.py
+++ code/internal/services/bepa.py
@@ -1,6 +1,8 @@
+import asyncio
 from datetime import datetime
 
 import requests
+import sqlalchemy as sa
 
 from code.internal.DB.database import get_db
 from code.internal.model.models import Price, AlertSubscription
@@ -9,6 +11,8 @@ from code.internal.model.models import Price, AlertSubscription
 MAILGUN_API_KEY = 'e88254122f9aadcfd8b789490578edaf-e5475b88-057fdc51'
 MAILGUN_DOMAIN = 'https://api.mailgun.net/v3/sandboxd810fa8fbc0944a39c6fb2760433b07d.mailgun.org'
 MAILGUN_SENDER = 'mailgun@sandboxd810fa8fbc0944a39c6fb2760433b07d.mailgun.org'
+
+is_bepa_service_running = False  # Flag to indicate if the bepa_service is running
 
 
 def fetch_latest_price(coin_name):
@@ -28,26 +32,34 @@ async def write_price_to_database(coin_name, price):
 
 
 async def bepa_service():
-    active_currencies = requests.get('http://localhost:8000/api/data').json()
-    list_of_coins = []
-    for coin_name in active_currencies:
-        price = fetch_latest_price(coin_name)
-        if price is not None:
-            await write_price_to_database(coin_name, price)
-            list_of_coins.append(coin_name)
+    global is_bepa_service_running
+    is_bepa_service_running = True
 
-    # Send email notifications to users based on alarm subscriptions
-    await send_email_notifications(list_of_coins)
+    while is_bepa_service_running:
+        active_currencies = requests.get('http://localhost:8000/api/data').json()
+        list_of_coins = []
+        for coin_name in active_currencies:
+            price = fetch_latest_price(coin_name)
+            if price is not None:
+                await write_price_to_database(coin_name, price)
+                list_of_coins.append(coin_name)
+
+        # Send email notifications to users based on alarm subscriptions
+        await send_email_notifications(list_of_coins)
+
+        await asyncio.sleep(2)  # Run the service every 60 seconds
 
 
 async def send_email_notifications(coins):
     async with get_db() as db:
         # Fetch users subscribed to the price changes of the given coins
-        subscriptions = await db.query(AlertSubscription).filter(AlertSubscription.coin_name.in_(coins)).all()
+        subscriptions = await db.execute(
+            sa.select(AlertSubscription).where(AlertSubscription.coin_name.in_(coins))
+        )
 
-        for subscription in subscriptions:
+        for subscription in subscriptions.scalars():
             # Calculate the percentage change of the subscribed coin
-            percentage_change = calculate_percentage_change(subscription.coin_name)
+            percentage_change = await calculate_percentage_change(subscription.coin_name)
 
             # Check if the current price change exceeds the subscribed difference percentage
             if abs(percentage_change) >= subscription.difference_percentage:
@@ -59,10 +71,13 @@ async def send_email_notifications(coins):
                 send_email(subscription.email, subject, message)
 
 
-def calculate_percentage_change(coin_name):
+async def calculate_percentage_change(coin_name):
     async with get_db() as db:
         # Fetch the last recorded price for the given coin
-        last_price = await db.query(Price).filter(Price.coin_name == coin_name).order_by(Price.timestamp.desc()).first()
+        last_price = await db.execute(
+            sa.select(Price).filter(Price.coin_name == coin_name).order_by(Price.timestamp.desc()).limit(1)
+        )
+        last_price = last_price.scalars().first()
 
         # Calculate the percentage change compared to the previous price
         if last_price is not None:
@@ -89,3 +104,9 @@ def send_email(recipient, subject, message):
         print(f"Email sent to {recipient}")
     else:
         print(f"Failed to send email to {recipient}")
+
+
+def stop_bepa_service():
+    global is_bepa_service_running
+    is_bepa_service_running = False
+    print("Bepa service stopped.")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -6,14 +7,11 @@ from fastapi.templating import Jinja2Templates
 
 from code.internal.DB.database import connect_to_database, close_database_connection, engine
 from code.internal.model.models import Base
-from code.internal.services.bepa import bepa_service
+from code.internal.services.bepa import bepa_service, stop_bepa_service
 from code.internal.services.peyk import app as peyk_app
 
 app = FastAPI()
 templates = Jinja2Templates(directory="code/templates")
-
-# Create a global variable to control the bepa_service task
-bepa_service_task = None
 
 
 async def create_tables():
@@ -26,15 +24,11 @@ async def startup_event():
     await connect_to_database()
     await connect_to_database()
     await create_tables()
-    # Start the bepa_service task upon startup
-    start_bepa_service()
 
 
 @app.on_event('shutdown')
 async def shutdown_event():
     await close_database_connection()
-    # Stop the bepa_service task upon shutdown
-    stop_bepa_service()
 
 
 # Home page
@@ -46,31 +40,14 @@ async def home(request: Request):
 # Include the Bepa and peyk services
 @app.get('/run_bepa_service')
 async def run_bepa_service():
-    start_bepa_service()
-    return {'result': 'Bepa service started'}
+    asyncio.create_task(bepa_service(), name='bepa_service')
+    return {'message': 'Bepa service started.'}
 
 
 @app.get('/stop_bepa_service')
-async def stop_bepa_service():
-    global bepa_service_task
-    if bepa_service_task is not None:
-        bepa_service_task.cancel()
-        bepa_service_task = None
-        return {'result': 'Bepa service stopped'}
-    else:
-        return {'result': 'Bepa service is not running'}
-
-
-async def bepa_service_wrapper():
-    while True:
-        await bepa_service()
-        await asyncio.sleep(60)  # Run every 60 seconds
-
-
-def start_bepa_service():
-    global bepa_service_task
-    if bepa_service_task is None:
-        bepa_service_task = asyncio.create_task(bepa_service_wrapper())
+async def stop_bepa_service_endpoint():
+    stop_bepa_service()
+    return {'message': 'Bepa service stopped.'}
 
 
 app.include_router(peyk_app)
