807ddedf0bb5474b00263bf3e8308d8b4a598519
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2023-06-28 17:46:13 +0330

    update main file for running bepa service periodically

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -11,6 +12,9 @@ from code.internal.services.peyk import app as peyk_app
 app = FastAPI()
 templates = Jinja2Templates(directory="code/templates")
 
+# Create a global variable to control the bepa_service task
+bepa_service_task = None
+
 
 async def create_tables():
     async with engine.begin() as conn:
@@ -22,11 +26,15 @@ async def startup_event():
     await connect_to_database()
     await connect_to_database()
     await create_tables()
+    # Start the bepa_service task upon startup
+    start_bepa_service()
 
 
 @app.on_event('shutdown')
 async def shutdown_event():
     await close_database_connection()
+    # Stop the bepa_service task upon shutdown
+    stop_bepa_service()
 
 
 # Home page
@@ -38,9 +46,31 @@ async def home(request: Request):
 # Include the Bepa and peyk services
 @app.get('/run_bepa_service')
 async def run_bepa_service():
-    result = await bepa_service()
-    # print("SALAM")
-    return {'result': result}
+    start_bepa_service()
+    return {'result': 'Bepa service started'}
+
+
+@app.get('/stop_bepa_service')
+async def stop_bepa_service():
+    global bepa_service_task
+    if bepa_service_task is not None:
+        bepa_service_task.cancel()
+        bepa_service_task = None
+        return {'result': 'Bepa service stopped'}
+    else:
+        return {'result': 'Bepa service is not running'}
+
+
+async def bepa_service_wrapper():
+    while True:
+        await bepa_service()
+        await asyncio.sleep(60)  # Run every 60 seconds
+
+
+def start_bepa_service():
+    global bepa_service_task
+    if bepa_service_task is None:
+        bepa_service_task = asyncio.create_task(bepa_service_wrapper())
 
 
 app.include_router(peyk_app)
